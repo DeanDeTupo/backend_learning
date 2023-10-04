f11797b88c581f20ff181b20655413ca7bed8729
DeanDeTupo <fedotovden882@gmail.com>
2023-10-04 23:16:44 +0300

    pidors added

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ const app = express();
 const port = 3003;
 
 app.get('/', (req: Request, res: Response) => {
+    let helloMessage = 'Hello World !';
+    res.send(helloMessage)
+})
+
+app.get('/pidors', (req: Request, res: Response) => {
     let helloMessage = 'Hello pidor !';
     res.send(helloMessage)
 })
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
